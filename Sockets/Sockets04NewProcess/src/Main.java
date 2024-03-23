6469971c770cbb743724934bb2ecc6f9b0148a8d
EduardoNunexx <eduardonunes@alu.ufc.br>
2024-03-23 13:56:58 -0300

    removing the client's responsabilitys

--- Sockets/Sockets04NewProcess/src/Main.java
+++ Sockets/Sockets04NewProcess/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Scanner sc = new Scanner(System.in);
+       String arg = sc.nextLine();
+       TCPClient.startClient(arg, 7896,"localhost");
     }
 }
--- Sockets/Sockets04NewProcess/src/TCPClient.java
+++ Sockets/Sockets04NewProcess/src/TCPClient.java
@@ -2,22 +2,19 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 public class TCPClient {
-	public static void main (String args[]) {
+	public static void startClient (String arg, int serverPort,String host) {
 	// arguments supply message and hostname of destination
 
 	Socket s = null;
 	    try{
-			Scanner sc = new Scanner(System.in);
-			String arg=sc.nextLine();
-	    	int serverPort = 7896;
-		   	s = new Socket("localhost", serverPort);
+
+		   	s = new Socket(host, serverPort);
 		   	DataInputStream in = new DataInputStream( s.getInputStream());
 			DataOutputStream out =
 				new DataOutputStream( s.getOutputStream());
 			out.writeUTF(arg);   
 			String data = in.readUTF();
-			System.out.println("Received: "+ data);      
-			sc.close();
+			System.out.println("Received: "+ data);
 	    } catch (UnknownHostException e){System.out.println("Sock:"+e.getMessage()); 
 	    } catch (EOFException e){ System.out.println("EOF:"+e.getMessage());
 	    } catch (IOException e){ System.out.println("IO:"+e.getMessage());
