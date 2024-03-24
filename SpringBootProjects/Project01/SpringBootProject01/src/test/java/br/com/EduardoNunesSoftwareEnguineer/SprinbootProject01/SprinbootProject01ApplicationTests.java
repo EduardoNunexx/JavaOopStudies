36dbd987e582390759b3ba52d88ab21f307fb3f7
EduardoNunexx <eduardonunes@alu.ufc.br>
2024-03-23 22:54:03 -0300

    Creating project

---
+++ SpringBootProjects/Project01/SpringBootProject01/src/test/java/br/com/EduardoNunesSoftwareEnguineer/SprinbootProject01/SprinbootProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.EduardoNunesSoftwareEnguineer.SprinbootProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
